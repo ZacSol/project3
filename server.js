accb91cf6d3b19eebb12a527b6739fd3c191cae5
ZacSol <42217556+ZacSol@users.noreply.github.com>
2018-12-03 10:54:49 -0600

    edit mongoDb connection

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ if (process.env.NODE_ENV === "production") {
 require('./routes/api-routes')(app);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recipeBox");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/recipeBox";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Start the API server
 app.listen(PORT, function() {
